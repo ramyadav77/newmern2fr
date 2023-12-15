d00c93ff26ce1f14e5a40a299181a17cf10c0200
Ram Pravesh <rampraveshyadav30122000@gmail.com>
2023-12-15 09:46:31 +0530

    adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Service from './pages/Service'
+import Contact from './pages/Contact'
+import Register from './pages/Register'
+import Login from './pages/login'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>after delete some part of app jsx</h1>
+     <BrowserRouter>
+     <Navbar/>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/service" element={<Service/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+
+
+
+
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
